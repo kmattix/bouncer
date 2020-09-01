a5b014b176de97208c881fd2487330e4404b4eeb
milkomeda99 <kmattrider@gmail.com>
2020-09-01 15:29:23 -0400

    autoRole handles user input now

--- src/milkomeda/bouncer/core/listeners/MessageReceived.java
+++ src/milkomeda/bouncer/core/listeners/MessageReceived.java
@@ -2,9 +2,13 @@ package milkomeda.bouncer.core.listeners;
 
 import milkomeda.bouncer.core.BouncerConfig;
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
 
 public class MessageReceived extends ListenerAdapter{
 
@@ -25,6 +29,32 @@ public class MessageReceived extends ListenerAdapter{
 	}
 
 	private void autoRole(MessageReceivedEvent event, String[] args){
-		System.out.println("autorole test");
+		MessageChannel channel = event.getChannel();
+		if(args.length == 1){
+			if(cfg.isAutoRoleEnabled())
+				channel.sendMessage("Auto role is enabled!").queue();
+			else
+				channel.sendMessage("Auto role is disabled!").queue();
+		}
+		else if(args[1].equalsIgnoreCase("disable")){
+			cfg.setAutoRoleID(null);
+			channel.sendMessage("Auto role is disabled!").queue();
+			System.out.println("Auto role is disabled");
+		}
+		else if(args.length == 2){
+			try{
+				Role role = event.getGuild().getRolesByName(args[1], true).get(0);
+				cfg.setAutoRoleID(role.getId());
+				channel.sendMessage(role.getAsMention() + " is now an auto role!").queue();
+				System.out.println(role.getName() + " is now an auto role");
+			}catch(IndexOutOfBoundsException e){
+				channel.sendMessage(args[1] + " is not a valid role!").queue();
+			}
+		}
+		try{
+			cfg.writeConfig();
+		}catch(IOException e){
+			e.printStackTrace();
+		}
 	}
 }
