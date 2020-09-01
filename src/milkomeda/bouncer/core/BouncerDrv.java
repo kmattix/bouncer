99ad068e262b6a3bf73baf1e0fbdd2ce60c1cb0c
milkomeda99 <kmattrider@gmail.com>
2020-09-01 10:20:13 -0400

    Initial commit

---
+++ src/milkomeda/bouncer/core/BouncerDrv.java
@@ -0,0 +1,23 @@
+package milkomeda.bouncer.core;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+import javax.security.auth.login.LoginException;
+
+public class BouncerDrv{
+
+	public static void main(String[] args) throws LoginException, InterruptedException{
+		if(args.length > 1){
+			System.out.println("Please provide a token as the first argument!");
+			System.exit(1);
+		}
+		JDA jda = JDABuilder.create(args[0], GatewayIntent.GUILD_MEMBERS)
+				.addEventListeners(new GuildMemberJoin())
+				.setActivity(Activity.watching("for new members"))
+				.build();
+		jda.awaitReady();
+	}
+}
---
+++ src/milkomeda/bouncer/core/GuildMemberJoin.java
@@ -0,0 +1,17 @@
+package milkomeda.bouncer.core;
+
+import net.dv8tion.jda.api.entities.Role;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import org.jetbrains.annotations.NotNull;
+
+public class GuildMemberJoin extends ListenerAdapter{
+
+	@Override
+	public void onGuildMemberJoin(@NotNull GuildMemberJoinEvent event){
+		System.out.println(event.getUser().getName() + " joined");
+		Role memberRole = event.getGuild().getRoleById("693344316470591508");
+		event.getGuild().addRoleToMember(event.getMember(), memberRole).queue();
+		System.out.println("Added " + memberRole.getName() + " to " + event.getUser().getName());
+	}
+}
