afe4ca44e5538bda933923ec9f669faa0b704e54
Kevin Mattix <kmattrider@gmail.com>
2020-09-01 15:28:41 -0400

    Gets parameters from the cfg

--- src/milkomeda/bouncer/core/listeners/GuildMemberJoin.java
+++ src/milkomeda/bouncer/core/listeners/GuildMemberJoin.java
@@ -1,5 +1,6 @@
 package milkomeda.bouncer.core.listeners;
 
+import milkomeda.bouncer.core.BouncerConfig;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
@@ -7,11 +8,21 @@ import org.jetbrains.annotations.NotNull;
 
 public class GuildMemberJoin extends ListenerAdapter{
 
+	private BouncerConfig cfg;
+
+	public GuildMemberJoin(BouncerConfig cfg){
+		this.cfg = cfg;
+	}
+
 	@Override
 	public void onGuildMemberJoin(@NotNull GuildMemberJoinEvent event){
-		System.out.println(event.getUser().getName() + " joined");
-		Role memberRole = event.getGuild().getRoleById("693344316470591508");
-		event.getGuild().addRoleToMember(event.getMember(), memberRole).queue();
-		System.out.println("Added " + memberRole.getName() + " to " + event.getUser().getName());
+		System.out.println(event.getUser().getAsTag() + " joined");
+		try{
+			Role autoRole = event.getGuild().getRoleById(cfg.getAutoRoleID());
+			event.getGuild().addRoleToMember(event.getMember(), autoRole).queue();
+			System.out.println("Added " + autoRole.getName() + " to " + event.getUser().getAsTag());
+		}catch(NumberFormatException e){
+			System.out.println("No auto role specified");
+		}
 	}
 }
