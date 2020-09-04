05b2340b7560b08db8b3f0e8901a7a69ddcfbd6d
milkomeda99 <kmattrider@gmail.com>
2020-09-03 20:48:13 -0400

    Added support for database and requires new arguments

--- src/milkomeda/bouncer/core/BouncerDrv.java
+++ src/milkomeda/bouncer/core/BouncerDrv.java
@@ -6,32 +6,25 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.entities.Activity;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 import javax.security.auth.login.LoginException;
-import java.io.File;
-import java.io.IOException;
 
 public class BouncerDrv{
 
-	public static void main(String[] args) throws LoginException, InterruptedException, IOException {
-		if(args.length != 1){
-			System.out.println("Please provide a token as the first argument!");
+	public static void main(String[] args) throws LoginException, InterruptedException{
+		if(args.length != 4){
+			System.out.println("Incorrect arguments");
 			System.exit(1);
 		}
 
-		BouncerConfig cfg;
-		File cfgFile = new File("bouncer.cfg");
-		if(cfgFile.exists()) {
-			cfg = BouncerConfig.readConfig(cfgFile);
-			System.out.println(cfgFile.getName() + " read");
-		}
-		else{
-			cfg = new BouncerConfig(cfgFile);
-			System.out.println(cfgFile.getName() + " created");
-		}
+		BouncerDB db = new BouncerDB(args[1], args[2], args[3]);
+		db.readDataBase();
 
 		JDA jda = JDABuilder.create(args[0], GatewayIntent.GUILD_MEMBERS, GatewayIntent.GUILD_MESSAGES)
-				.addEventListeners(new GuildMemberJoin(cfg), new MessageReceived(cfg), new GuildMemberLeave())
-				.setActivity(Activity.watching("for new members"))
+				.addEventListeners(new GuildJoin(db), new GuildLeave(db), new GuildUpdateName(db))
+				.addEventListeners(new GuildMemberJoin(db), new MessageReceived(db))
+				.setActivity(Activity.watching("new members | !help"))
 				.build();
 		jda.awaitReady();
+
+		// TODO: 9/3/2020 verify that there are no data base entries the JDA is not servicing
 	}
 }
