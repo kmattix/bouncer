5388cf1ec50285604abe1a2c623d63d7b705157a
milkomeda99 <kmattrider@gmail.com>
2020-09-05 21:48:21 -0400

    DMs owner now on a HierarchyException

--- src/milkomeda/bouncer/core/listeners/GuildMemberJoin.java
+++ src/milkomeda/bouncer/core/listeners/GuildMemberJoin.java
@@ -4,6 +4,7 @@ import milkomeda.bouncer.core.BouncerDB;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Role;
 import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent;
+import net.dv8tion.jda.api.exceptions.HierarchyException;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
@@ -18,7 +19,16 @@ public class GuildMemberJoin extends ListenerAdapter{
 	@Override
 	public void onGuildMemberJoin(@NotNull GuildMemberJoinEvent event){
 		Guild guild = event.getGuild();
-		Role autoRole = event.getGuild().getRoleById(db.getRoleID(guild.getIdLong()));
-		event.getGuild().addRoleToMember(event.getMember(), autoRole).queue();
+		if(db.getRoleID(guild.getIdLong()) != 0){
+			Role autoRole = event.getGuild().getRoleById(db.getRoleID(guild.getIdLong()));
+			try{
+				event.getGuild().addRoleToMember(event.getMember(), autoRole).queue();
+			}catch(HierarchyException e){
+				guild.getOwner().getUser().openPrivateChannel().flatMap(pChannel -> pChannel.sendMessage(
+						"Verify the bouncer role is above your '" + autoRole.getName() + "' auto role" +
+								" in " + db.getGuildName(guild.getIdLong()) + ". " + event.getUser().getAsTag() + " was" +
+								" not added to the auto role.")).queue();
+			}
+		}
 	}
 }
