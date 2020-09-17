d4e1e31fd61380cb8211df55b2e532ebff73e771
milkomeda99 <kmattrider@gmail.com>
2020-09-17 18:21:56 -0400

    Documentation

--- src/milkomeda/bouncer/core/listeners/guild/GuildJoin.java
+++ src/milkomeda/bouncer/core/listeners/guild/GuildJoin.java
@@ -5,14 +5,23 @@ import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Class listener for when a guild is added to the JDA. Purpose is to initialize it to the database.
+ */
 public class GuildJoin extends ListenerAdapter{
 
 	private final BouncerDB DB;
 
+	/**
+	 * @param db Database connection.
+	 */
 	public GuildJoin(BouncerDB db){
 		DB = db;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void onGuildJoin(@NotNull net.dv8tion.jda.api.events.guild.GuildJoinEvent event){
 		Guild guild = event.getGuild();
