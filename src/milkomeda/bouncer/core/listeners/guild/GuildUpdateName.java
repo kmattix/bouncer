c110fc67f7d2456c88567535b6a5b55e817428f2
milkomeda99 <kmattrider@gmail.com>
2020-09-17 18:13:33 -0400

    Documentation

--- src/milkomeda/bouncer/core/listeners/guild/GuildUpdateName.java
+++ src/milkomeda/bouncer/core/listeners/guild/GuildUpdateName.java
@@ -6,14 +6,24 @@ import net.dv8tion.jda.api.events.guild.update.GuildUpdateNameEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Class listener for when a guild changes its name. Purpose is to update the databases entry in case a guild changes
+ * its name.
+ */
 public class GuildUpdateName extends ListenerAdapter{
 
 	private final BouncerDB DB;
 
+	/**
+	 * @param db Database connection.
+	 */
 	public GuildUpdateName(BouncerDB db){
 		DB = db;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void onGuildUpdateName(@NotNull GuildUpdateNameEvent event){
 		Guild guild = event.getGuild();
