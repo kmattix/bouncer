87f423f66b6cd7d12709f79c1a972db60b8ab1d8
milkomeda99 <kmattrider@gmail.com>
2020-09-17 18:18:24 -0400

    Documentation

--- src/milkomeda/bouncer/core/listeners/guild/GuildLeave.java
+++ src/milkomeda/bouncer/core/listeners/guild/GuildLeave.java
@@ -6,14 +6,24 @@ import net.dv8tion.jda.api.events.guild.GuildLeaveEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import org.jetbrains.annotations.NotNull;
 
+/**
+ * Class listener for when a guild leaves the JDA. Purpose is to remove the guild from the database to save database
+ * resources.
+ */
 public class GuildLeave extends ListenerAdapter{
 
 	private final BouncerDB DB;
 
+	/**
+	 * @param db Database connection.
+	 */
 	public GuildLeave(BouncerDB db){
 		DB = db;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void onGuildLeave(@NotNull GuildLeaveEvent event){
 		Guild guild = event.getGuild();
